85a192a8acec2ec66adc214a8622c3ebfdcdeaf2
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-02-26 16:16:23 +0000

    + [php] 1. Two Sum

--- php/1--two-sum.php
+++ php/1--two-sum.php
@@ -1,5 +1,6 @@
 <?php
 //  1. Two Sum
+// https://leetcode.com/problems/two-sum/
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
@@ -44,7 +45,7 @@ class Solution
      * @param Integer $target
      * @return Integer[]
      */
-    function twoSum($nums, $target): array
+    function twoSum(array $nums, int $target): array
     {
         $map = [];
         foreach ($nums as $i => $num) {
