d63fe5a90226a1b0d4ebbcc4de2b637bf4da1a79
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-03-09 00:32:40 +0000

    + [javascript] 142. Linked List Cycle II

---
+++ js/142--linked-list-cycle-ii.js
@@ -0,0 +1,75 @@
+// 142. Linked List Cycle II
+// https://leetcode.com/problems/linked-list-cycle-ii/
+// Medium
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+// Do not modify the linked list.
+//
+//Example 1:
+//Input: head = [3,2,0,-4], pos = 1
+//Output: tail connects to node index 1
+//Explanation: There is a cycle in the linked list, where tail connects to the second node.
+//Example 2:
+//Input: head = [1,2], pos = 0
+//Output: tail connects to node index 0
+//Explanation: There is a cycle in the linked list, where tail connects to the first node.
+//Example 3:
+//Input: head = [1], pos = -1
+//Output: no cycle
+//Explanation: There is no cycle in the linked list.
+//
+//Constraints:
+//The number of the nodes in the list is in the range [0, 104].
+//-105 <= Node.val <= 105
+//pos is -1 or a valid index in the linked-list.
+//Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// var detectCycle = function(head) {
+//     let p1 = head, p2 = head;
+//
+//     while(p2 != null && p2.next != null){
+//         p1 = p1.next;
+//         p2 = p2.next.next;
+//         if(p1 === p2) break;
+//     }
+//
+//     if(p2 === null || p2.next === null) return null;
+//     while(head != p1){
+//         head = head.next;
+//         p1 = p1.next;
+//     }
+//     return head;
+// };
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+    let s = head;
+    let f = head;
+    while (f && f.next && f.next.next) {
+        s = s.next;
+        f = f.next.next;
+        if (s === f) {
+            while (s !== head) {
+                head = head.next;
+                s = s.next;
+            }
+            return s;
+        }
+    }
+    return null;
+};
