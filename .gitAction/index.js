79c6ab7c0be62d521fcfaba972a81ead0b22ccbd
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-03-19 16:09:10 +0000

    update Action Script

--- .gitAction/index.js
+++ .gitAction/index.js
@@ -1,9 +1,11 @@
 const core = require("@actions/core");
 const {Octokit} = require('@octokit/core');
+const fs = require('fs');
 
 // const process = require('./config');
 // const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 // const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN', {required: true});
 const GITHUB_REPOSITORY = core.getInput('GITHUB_REPOSITORY', {required: true});
 
@@ -21,6 +23,42 @@ const getRequest = async (url) => {
         throw new Error(e.message);
     }
 }
+
+// function get all Leetcode Problems from API or local json leetcode.problems.json file
+const getLeetcodeProblemsListJsonData = async (url) => {
+    url = url || 'https://leetcode.com/api/problems/all/';
+    let localPathToLeetcodeProblems = '../leetcode.problems.all.json';
+    // try to open file from url first https://leetcode.com/api/problems/all/
+    // if not found, try to open file from local
+    let leetcodeProblemsJsonData;
+    console.log('Get leetcode problems. ' + url);
+    try {
+        res = await octokit.request(url, {
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+        console.log('Get leetcode problems successful.' + url);
+    } catch (e) {
+        console.error("Get leetcode problems failed, with error: ", e.message);
+    }
+    if (res.status !== 200) {
+        console.log('Get leetcode problems failed. ' + url);
+        // open file from local
+        // read file FS
+        leetcodeProblemsJsonData = fs.readFileSync(localPathToLeetcodeProblems, 'utf8');
+        // leetcodeProblemsJsonData = await new Promise((done, reject) => {
+        //     fs.readFile(localPathToLeetcodeProblems, 'utf8', (err, data) => {
+        //         done(data);
+        //     });
+        // });
+    } else {
+        leetcodeProblemsJsonData = res.data;
+    }
+    return leetcodeProblemsJsonData;
+}
+
+
 const updateReadme = async (url, requestOptions) => {
     console.log('Update readme. ' + url);
     try {
@@ -52,10 +90,11 @@ const getSolvedProblemArray = async (url, checkFolderArr = ['php', 'js']) => {
             let fileName = value.name;
             let fileUrl = value.html_url;
 
-            var rowSplit = fileName.split("--");
-            var num = String(rowSplit[0].replace(/^0+/, ''));
-            var leetURL = String(rowSplit[1]).replace('.' + lang, '');
-            var name = toTitleCase(rowSplit[1].replace(/\-/g, ' ').replace('.' + lang, '').toUpperCase());
+            let rowSplit = fileName.split("--");
+            let num = String(rowSplit[0].replace(/^0+/, ''));
+            let leetURL = String(rowSplit[1]).replace('.' + lang, '');
+            let name = toTitleCase(rowSplit[1].replace(/-/g, ' ').replace('.' + lang, '').toUpperCase());
+            console.log(name);
             if (Object.keys(solvedProblemsArray).indexOf(num) === -1) {
                 solvedProblemsArray[num] = {};
                 solvedProblemsArray[num]['num'] = num.padStart(4, '0');
@@ -69,6 +108,27 @@ const getSolvedProblemArray = async (url, checkFolderArr = ['php', 'js']) => {
     return solvedProblemsArray;
 }
 
+async function getProblemMetaInfo(problemsMetaJson, problemID) {
+    let res = {
+        question_id: 0,
+        question_title: '',
+        question_title_slug: '',
+        difficulty: '',
+    };
+    let problemMeta = null;
+    if (problemsMetaJson && problemID) {
+        problemMeta = await problemsMetaJson.stat_status_pairs.find(element => element.stat.frontend_question_id === parseInt(problemID));
+    }
+    if (!problemMeta) {
+        return res;
+    }
+    res.question_id = problemMeta.stat.frontend_question_id;
+    res.question_title = problemMeta.stat.question__title;
+    res.question_title_slug = problemMeta.stat.question__title_slug;
+    res.difficulty = problemMeta.difficulty.level;
+    return res;
+}
+
 (async () => {
     try {
         const username = GITHUB_REPOSITORY.split("/")[0];
@@ -76,26 +136,42 @@ const getSolvedProblemArray = async (url, checkFolderArr = ['php', 'js']) => {
         const readmePath = GITHUB_REPOSITORY.split("/")[2];
 
         console.log(`Job begin at: ${new Date()}`);
-        const lastCommits = await getRequest(`GET /repos/${username}/${repo}/commits`)
-        for (const value of lastCommits.data) {
-            let lastCommit = value.commit.message;
-            let lastCommitDate = value.commit.author.date;
-            let lastCommitUrl = value.html_url;
-        }
+        // const lastCommits = await getRequest(`GET /repos/${username}/${repo}/commits`)
+        // for (const value of lastCommits.data) {
+        //     let lastCommit = value.commit.message;
+        //     let lastCommitDate = value.commit.author.date;
+        //     let lastCommitUrl = value.html_url;
+        // }
+
+
+        const problemsMetaJson = JSON.parse(await getLeetcodeProblemsListJsonData());
+        // console.log(problemsMetaJson);
+        console.log('Get solved problems.');
+
+        //  variable 1= easy, 2= medium, 3= hard
+        const Difficulty = {
+            1: 'Easy',
+            2: 'Medium',
+            3: 'Hard'
+        };
 
         let solvedProblems = await getSolvedProblemArray(`GET /repos/${username}/${repo}/contents`);
-        let readmeContentData = '\n' +
-            '|   #   | Name  | JS   | PHP  |\n' +
-            '|-------|-------|------|------|\n';
+        let readmeContentData = '\n' + '|   #   | Name  | Difficulty | JS   | PHP  |\n' + '|-------|-------|-------|------|------|\n';
         for (const problemID in solvedProblems) {
+            let problemInfoData = await getProblemMetaInfo(problemsMetaJson, problemID);
+
             let row = solvedProblems[problemID];
+            let problemName = problemInfoData.question_title ? problemInfoData.question_title : row['name'];
+            // let problemSlug = problemInfoData.question_title_slug ? problemInfoData.question_title_slug : row['leetURL'];
+            let problemDifficulty = problemInfoData.difficulty ? Difficulty[problemInfoData.difficulty] : '';
+
             if (!row['js']) {
                 row['jsURL'] = 'x';
             } else row['jsURL'] = '[JS](' + row['jsURL'] + ')';
             if (!row['php']) {
                 row['phpURL'] = 'x';
             } else row['phpURL'] = '[PHP](' + row['phpURL'] + ')';
-            let rowStr = `| ${row['num']} |   [${row['name']}](https://leetcode.com/problems/${row['leetURL']}/)   |  ${row['jsURL']}  |  ${row['phpURL']}  |\n`;
+            let rowStr = `|<sup>${row['num']}</sup>|<sup>[${problemName}](https://leetcode.com/problems/${row['leetURL']}/)</sup>|<sup>${problemDifficulty}</sup>|<sup>${row['jsURL']}</sup>|<sup>${row['phpURL']}</sup>|\n`;
             readmeContentData += rowStr;
         }
 
@@ -106,7 +182,7 @@ const getSolvedProblemArray = async (url, checkFolderArr = ['php', 'js']) => {
         console.log('Get README.md.');
         const readmeTextSource = await getRequest(`GET /repos/${username}/${repo}/contents/${readmePath}`);
         const sha1 = readmeTextSource.data.sha;
-
+        // replace Solutions Table in README.md
         let decodedContent = Buffer.from(readmeTextSource.data.content, "base64").toString('utf8');
         readmeContentData = decodedContent.replace(/<!-- LeetCode Solutions Table -->[\s\S]*<!-- End LeetCode Solutions of Table -->/, `<!-- LeetCode Solutions Table -->${readmeContentData}<!-- End LeetCode Solutions of Table -->`);
 
@@ -120,7 +196,7 @@ const getSolvedProblemArray = async (url, checkFolderArr = ['php', 'js']) => {
 
         console.log(`Job complete at: ${new Date()}`);
     } catch (e) {
-        console.error("Error occrence with error: ", e.message)
-        core.setFailed("Failed: ", e.message)
+        console.error("Error occurrence with error: ", e.message)
+        core.setFailed("Failed: " + e.message)
     }
 })()
