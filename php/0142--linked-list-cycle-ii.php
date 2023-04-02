e22c0c56ee1ad2b4cb15d7423e76e6b543251b93
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-04-02 21:46:42 +0100

    + [.php] 142. Linked List Cycle II

--- php/0142--linked-list-cycle-ii.php
+++ php/0142--linked-list-cycle-ii.php
@@ -5,26 +5,23 @@
 // Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
 // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
 // Do not modify the linked list.
-//
-//Example 1:
-//Input: head = [3,2,0,-4], pos = 1
-//Output: tail connects to node index 1
-//Explanation: There is a cycle in the linked list, where tail connects to the second node.
-//Example 2:
-//Input: head = [1,2], pos = 0
-//Output: tail connects to node index 0
-//Explanation: There is a cycle in the linked list, where tail connects to the first node.
-//Example 3:
-//Input: head = [1], pos = -1
-//Output: no cycle
-//Explanation: There is no cycle in the linked list.
-//
-//Constraints:
-//The number of the nodes in the list is in the range [0, 104].
-//-105 <= Node.val <= 105
-//pos is -1 or a valid index in the linked-list.
-//Follow up: Can you solve it using O(1) (i.e. constant) memory?
-
+//    Example 1:
+//    Input: head = [3,2,0,-4], pos = 1
+//    Output: tail connects to node index 1
+//    Explanation: There is a cycle in the linked list, where tail connects to the second node.
+//    Example 2:
+//    Input: head = [1,2], pos = 0
+//    Output: tail connects to node index 0
+//    Explanation: There is a cycle in the linked list, where tail connects to the first node.
+//    Example 3:
+//    Input: head = [1], pos = -1
+//    Output: no cycle
+//    Explanation: There is no cycle in the linked list.
+//    Constraints:
+//    The number of the nodes in the list is in the range [0, 104].
+//    -105 <= Node.val <= 105
+//    pos is -1 or a valid index in the linked-list.
+//    Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
 /**
  * Definition for a singly-linked list.
@@ -40,8 +37,7 @@ class Solution
      * @param ListNode $head
      * @return ListNode
      */
-    function detectCycle($head)
-    {
+    function detectCycle($head) {
         $s = $head;
         $f = $head;
         while ($f != null && $f->next != null) {
@@ -59,8 +55,7 @@ class Solution
         return null;
     }
 
-    function detectCycle1($head)
-    {
+    function detectCycle1($head) {
         $fast = $head;
         $slow = $head;
         while ($fast && $fast->next) {
@@ -97,8 +92,7 @@ class ListNode
     public $val = 0;
     public $next = null;
 
-    function __construct($val = 0, $next = null)
-    {
+    function __construct($val = 0, $next = null) {
         $this->val = $val;
         $this->next = $next;
     }
@@ -108,14 +102,12 @@ class LinkedList
 {
     public $head;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->head = null;
     }
 
     //Add element at the start of the list
-    public function pushFront($newElement)
-    {
+    public function pushFront($newElement) {
         //1. allocate a new node
         $newNode = new ListNode();
         //2. assign data element
@@ -126,8 +118,7 @@ class LinkedList
         $this->head = $newNode;
     }
 
-    public function pushBack($newElement)
-    {
+    public function pushBack($newElement) {
         //1. allocate node
         $newNode = new ListNode();
         //2. assign data element
@@ -151,8 +142,7 @@ class LinkedList
     }
 
     // array to linked list
-    public function arrayToLinkedList($array)
-    {
+    public function arrayToLinkedList($array) {
         $this->head = null;
         foreach ($array as $value) {
             $this->pushBack($value);
@@ -160,8 +150,7 @@ class LinkedList
     }
 
     //display the content of the list
-    public function printList()
-    {
+    public function printList() {
         $temp = new ListNode();
         $temp = $this->head;
         if ($temp != null) {
