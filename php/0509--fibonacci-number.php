515a41a24a26652a62ff0e4fd06dd1b71628f970
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-04-05 19:58:00 +0100

    + [.php] 509. Fibonacci Number

--- php/0509--fibonacci-number.php
+++ php/0509--fibonacci-number.php
@@ -1,38 +1,35 @@
 <?php
 // 509. Fibonacci Number
 // https://leetcode.com/problems/fibonacci-number/description/
-//Easy
-//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
-//F(0) = 0, F(1) = 1
-//F(n) = F(n - 1) + F(n - 2), for n > 1.
-//Given n, calculate F(n).
+// Easy
+//    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+//    F(0) = 0, F(1) = 1
+//    F(n) = F(n - 1) + F(n - 2), for n > 1.
+//    Given n, calculate F(n).
 //
-//Example 1:
-//Input: n = 2
-//Output: 1
-//Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
-//Example 2:
-//Input: n = 3
-//Output: 2
-//Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
-//Example 3:
-//Input: n = 4
-//Output: 3
-//Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
-//
-//
-//Constraints:
-//0 <= n <= 30
+//    Example 1:
+//    Input: n = 2
+//    Output: 1
+//    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+//    Example 2:
+//    Input: n = 3
+//    Output: 2
+//    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+//    Example 3:
+//    Input: n = 4
+//    Output: 3
+//    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+//    Constraints:
+//    0 <= n <= 30
+
 class Solution
 {
     /**
      * @param Integer $n
      * @return Integer
      */
-    function fib(int $n): int
-    {
-        //Constraints:
-        //0 <= n <= 30
+    function fib(int $n): int {
+        // Constraints: 0 <= n <= 30
         // $answersLOL = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040];
         // return $answersLOL[$n];
 
@@ -74,8 +71,7 @@ foreach ($cases as $case) {
  * @param $expectedOutput
  * @return void
  */
-function echoResult($result, $expectedOutput): void
-{
+function echoResult($result, $expectedOutput): void {
     echo '<pre>' . '--------' . PHP_EOL . 'Result:' . PHP_EOL;
     var_export($result);
     echo PHP_EOL . 'Valid/Expected Output is:' . PHP_EOL;
