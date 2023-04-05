ba967d56aedad65a889744f9114fb7f5db999e78
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-04-05 18:58:57 +0100

    + [.js] 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ js/0235--lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,62 @@
+// 235. Lowest Common Ancestor of a Binary Search Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+// Medium
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+//
+// Example 1:
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
+// Explanation: The LCA of nodes 2 and 8 is 6.
+// Example 2:
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+// Output: 2
+// Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+// Example 3:
+// Input: root = [2,1], p = 2, q = 1
+// Output: 2
+//
+// Constraints:
+// The number of nodes in the tree is in the range [2, 105].
+// -109 <= Node.val <= 109
+// All Node.val are unique.
+// p != q
+// p and q will exist in the BST.
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+// recursive approach
+var lowestCommonAncestor = function (root, p, q) {
+    // both p and q are in the left subtree
+    if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+    // both p and q are in the right subtree
+    if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+
+    return root;
+};
+
+
+// -- OR --
+
+// iterative approach
+var lowestCommonAncestor = function (root, p, q) {
+    while (root !== null) {
+        if (root.val > p.val && root.val > q.val) {
+            root = root.left
+        } else if (root.val < p.val && root.val < q.val) {
+            root = root.right
+        } else {
+            return root;
+        }
+    }
+    return null
+};
