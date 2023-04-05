847e38d2b67137502a4aae3cca21c605031dd988
akunopaka <7946748+akunopaka@users.noreply.github.com>
2023-04-05 17:50:37 +0100

    + [.php] 98. Validate Binary Search Tree

--- php/0098--validate-binary-search-tree.php
+++ php/0098--validate-binary-search-tree.php
@@ -1,25 +1,25 @@
 <?php
-//98. Validate Binary Search Tree
-//https://leetcode.com/problems/validate-binary-search-tree/
-//Medium
-//Given the root of a binary tree, determine if it is a valid binary search tree (BST).
-//A valid BST is defined as follows:
-//The left
-//subtree
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+// Medium
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+// A valid BST is defined as follows:
+// The left
+// subtree
 // of a node contains only nodes with keys less than the node's key.
-//The right subtree of a node contains only nodes with keys greater than the node's key.
-//Both the left and right subtrees must also be binary search trees.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
 //
-//Example 1:
-//Input: root = [2,1,3]
-//Output: true
-//Example 2:
-//Input: root = [5,1,4,null,null,3,6]
-//Output: false
-//Explanation: The root node's value is 5 but its right child's value is 4.
-//Constraints:
-//The number of nodes in the tree is in the range [1, 104].
-//-231 <= Node.val <= 231 - 1
+// Example 1:
+// Input: root = [2,1,3]
+// Output: true
+// Example 2:
+// Input: root = [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4.
+// Constraints:
+// The number of nodes in the tree is in the range [1, 104].
+// -231 <= Node.val <= 231 - 1
 
 /**
  * Definition for a binary tree node.
@@ -39,8 +39,7 @@ class TreeNode
     public $val = null;
     public $left = null;
 
-    function __construct($val = 0, $left = null, $right = null)
-    {
+    function __construct($val = 0, $left = null, $right = null) {
         $this->val = $val;
         $this->left = $left;
         $this->right = $right;
@@ -55,8 +54,7 @@ class Solution
      * @param TreeNode $root
      * @return Boolean
      */
-    function isValidBST($root, $minLimit = null, $maxLimit = null)
-    {
+    function isValidBST($root, $minLimit = null, $maxLimit = null) {
         if ($root === null) return true;
         if (($minLimit !== null && $root->val <= $minLimit) ||
             ($maxLimit !== null && $root->val >= $maxLimit)) {
@@ -76,13 +74,11 @@ class Solution_______2
      * @param TreeNode $root
      * @return Boolean
      */
-    function isValidBST($root)
-    {
+    function isValidBST($root) {
         return $this->isNodeValid($root, null, null);
     }
 
-    function isNodeValid($root, $minLimit, $maxLimit)
-    {
+    function isNodeValid($root, $minLimit, $maxLimit) {
         if ($root === null) {
             return true;
         }
@@ -93,20 +89,18 @@ class Solution_______2
     }
 }
 
-class Solution_akuno_trash
+class Solution___a_trash
 {
 
     /**
      * @param TreeNode $root
      * @return Boolean
      */
-    function isValidBST(TreeNode $root)
-    {
+    function isValidBST(TreeNode $root) {
         return $this->isNodeValid($root);
     }
 
-    function isNodeValid(TreeNode $root, $lower = null, $greater = null)
-    {
+    function isNodeValid(TreeNode $root, $lower = null, $greater = null) {
 //        echo '<pre>';
 //        echo 'R: ' . $root->val . PHP_EOL;
 //        echo 'L: ' . $lower . PHP_EOL;
@@ -154,13 +148,7 @@ echo '</pre>';
 //1
 //0
 
-//I hope you find it helpful .
 //
-//Cheers
-//
-//PHP Code: // class with sample methods treeNodes class Solution { /** * @param TreeNode $root * @return Boolean */ function isValidBST ( $root ) { return $this -> validate ( $root , null , null ) ; } function validate ( $root , $min , $max ) { if ( $root === null ) { return true ; } if ( $min !== null && $root -> val <= $min ) { return false ; } if ( $max !== null && $root -> val >= $max ) { return false ; } return $this -> validate ( $root -> left , $min , $root -> val ) && $this -> validate ( $root -> right , $root -> val , $max ) ; } }
-//
-//PHP Code:
 //$root = new TreeNode (2, new TreeNode (1), new TreeNode (3));
 //$root1 = new TreeNode (5, new TreeNode (1), new TreeNode (4, new TreeNode (3), new TreeNode (6)));
 //$solution = new Solution ();
